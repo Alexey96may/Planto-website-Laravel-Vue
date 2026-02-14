703a1031769263de1c8f5dff1efff9c12ef0148b
Alexey96may <shulga_alexey@vk.com>
2026-02-14 22:03:09 +0300

    Migrate and adjust icons

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
     plugins: [
@@ -16,5 +19,12 @@ export default defineConfig({
                 },
             },
         }),
+        svgLoader(),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./resources/js"),
+            img: path.resolve(__dirname, "./resources/images"),
+        },
+    },
 });
