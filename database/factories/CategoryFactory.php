aeb9729d5eb4c0fc038c14609412f1abbc205375
Alexey96may <shulga_alexey@vk.com>
2026-03-27 20:24:48 +0300

    fix: faker() function

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+use Faker\Factory as Faker;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
@@ -16,8 +18,11 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        
+        $faker = \Faker\Factory::create();
+        
         return [
-            'title' => $this->faker->unique()->words(2, true),
+            'title' => $faker->unique()->words(2, true),
             'slug' => fn (array $attributes) => \Illuminate\Support\Str::slug($attributes['title']),
         ];
     }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -16,12 +17,14 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = \Faker\Factory::create();
+
         return [
-            'title' => $this->faker->words(3, true),
-            'description' => $this->faker->paragraph(),
-            'price' => $this->faker->randomFloat(2, 10, 5000),
-            'stock' => $this->faker->numberBetween(0, 50),
-            'is_trending' => $this->faker->boolean(20),
+            'title' => $faker->words(3, true),
+            'description' => $faker->paragraph(),
+            'price' => $faker->randomFloat(2, 10, 5000),
+            'stock' => $faker->numberBetween(0, 50),
+            'is_trending' => $faker->boolean(20),
             'trending_order' => 0,
             'category_id' => \App\Models\Category::factory(), 
             'image'       => 'products/test-flower.jpg',
